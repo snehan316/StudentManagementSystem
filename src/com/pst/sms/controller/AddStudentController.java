15bfc9962c77cd3fd8b8e8a8bd0df8389d50f7c2
snehan316 <63293229+snehan316@users.noreply.github.com>
2022-01-23 20:26:28 +0530

    Added view and delete functionalities

---
+++ src/com/pst/sms/controller/AddStudentController.java
@@ -0,0 +1,47 @@
+package com.pst.sms.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.pst.sms.VO.StudentVO;
+import com.pst.sms.service.StudentService;
+
+/**
+ * Servlet implementation class AddStudentController
+ */
+@WebServlet("/AddStudentController")
+public class AddStudentController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+  	StudentVO studentVO = null;
+  	StudentService studentService =null;
+  	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		int rollNumber =Integer.parseInt( request.getParameter("roll_number"));
+		String name = request.getParameter("name");
+		String gender = request.getParameter("gender");
+		String course = request.getParameter("course");
+		
+		studentVO = new StudentVO();
+		studentVO.setRollNumber(rollNumber);
+		studentVO.setName(name);
+		studentVO.setGender(gender);
+		studentVO.setCourse(course);
+		
+		studentService = new StudentService();
+		int i = studentService.saveData(studentVO);
+		
+		if( i != 0) {
+			String msg = "Successfully added";
+			request.setAttribute("msg", msg);
+			request.getRequestDispatcher("./add_student.jsp").forward(request, response);
+		}
+
+	}
+}
